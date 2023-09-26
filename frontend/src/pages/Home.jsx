050325842c162fe5300b49735234c9bd1db36e4c
David Olsson <david.olsson@elev.ntig.se>
2023-09-26 10:45:30 +0200

    Added README.md guide and small examples

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,11 @@
 const Home = () => {
-    return <>Home</>;
+    return (
+        <>
+            <div id="home">
+                <div className="test-shadow-box">Home</div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
