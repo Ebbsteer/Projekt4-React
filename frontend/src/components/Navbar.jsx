083ca8c0e6f4249c79804b5d0b3842d189c30b05
zimonjohnsson <zimonjohnsson2004@gmail.com>
2023-09-27 12:14:33 +0200

    kjhgfdsa

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ const Navbar = () => {
     <div id="nav">
         <div id="navleft">
             <a href="">hej</a>
-            <a href="">hej</a>
-            <a href="">hej</a>
-            <a href="">hej</a>
+            <a href="">HEM</a>
+            <a href="">ITEMS</a>
+            <a href="">PLANETER&#8681;</a>
         </div>
             <div id="navright">
                 <a href="">&#9733;</a>
-                <a href="">Hje</a>
+                <a href="">&#9817;</a>
             </div>
     </div>
     </>;
