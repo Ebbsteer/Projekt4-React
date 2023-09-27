cb26c6b6fe81dd7333bc92ef9e12f812ece464c0
zimonjohnsson <zimonjohnsson2004@gmail.com>
2023-09-27 13:28:07 +0200

    nmbvcfxsredtrfgbv

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,23 @@
+import {useState} from 'react';
+
 const Navbar = () => {
+    const [dropdown, setDropdown] = useState(true);
+
     return <>
     <div id="nav">
-        <div id="navleft">
-            <a href=""><img src="../src/assets/img/logospace.jpg" alt="" /></a>
-            <a href="">HOME</a>
-            <a href="">ITEMS</a>
-            <a href="">PLANETS&#8681;</a>
-        </div>
+        <button onClick={()=>{setDropdown(!dropdown)}}>Hej</button>
+        <div id="droppa">
+            <div id="navleft">
+                <a href=""><img src="../src/assets/img/logospace.jpg" alt="" /></a>
+                <a href="">HOME</a>
+                <a href="">ITEMS</a>
+                <a href="">PLANETS&#8681;</a>
+            </div>
             <div id="navright">
                 <a href="">&#9733;</a>
                 <a href="">&#9817;</a>
             </div>
+        </div>
     </div>
     </>;
 };
