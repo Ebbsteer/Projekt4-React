f8988d4de6975b59721d397fe390d7a1c38cbc9f
zimonjohnsson <zimonjohnsson2004@gmail.com>
2023-09-27 12:02:50 +0200

    l

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 const Navbar = () => {
     return <>
     <div id="nav">
+        <div id="navleft">
             <a href="">hej</a>
             <a href="">hej</a>
             <a href="">hej</a>
             <a href="">hej</a>
+        </div>
+            <div id="navright">
+                <a href="">&#9733;</a>
+                <a href="">Hje</a>
+            </div>
     </div>
     </>;
 };
