c8848e472ffc97502f8ce292a33bf4b65965fbf5
Salah Al Hassun <142593904+Salah9320Alhassun@users.noreply.github.com>
2023-09-27 14:49:54 +0200

    info box home page half done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import nebulae from "../assets/img/nebulae.jpeg";
 
+
 const Home = () => {
   return (
-    <>
-        <div id="home">
-
-            
-            <div className="nebulae">
-                {/* <img className="nebaulae" alt="nebaulae" src={nebulae} draggable={false}/> */}
-            </div>
-        </div>
-    </>
+    <div id="home">
+      <div className="nebulae">
+        {/* <img className="nebaulae" alt="nebaulae" src={nebulae} draggable={false}/> */}
+      </div>
+      <div className="Homebox">
+        <input type="text" className="textbox" placeholder="Write something here..." />
+      </div>
+    </div>
   );
 };
 
