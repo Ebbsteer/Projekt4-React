2952d7b682a3ac1903bfdd4a1e1ba8d9dd898b29
zimonjohnsson <zimonjohnsson2004@gmail.com>
2023-09-28 12:20:36 +0200

    hfdgs

--- frontend/src/pages/items.jsx
+++ frontend/src/pages/items.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react"
 
 const Items = () => {
     const {id} = useParams() 
     console.log(id);
+
+    const itemList=("https://api.le-systeme-solaire.net/rest/bodies/")
+
+    const [planets, setPlanets] = useState([])
+  
+    const fetchUserData = () => {
+      fetch(itemList)
+        .then(response => {
+          return response.json()
+        })
+        .then(data => {
+          console.log(data);
+          setPlanets(data.bodies);
+        })
+    }
+  
+    useEffect(() => {
+      fetchUserData()
+    }, [])
 
 
   return (
@@ -11,8 +31,20 @@ const Items = () => {
       <div className="nebulae">
         {/* <img className="nebaulae" alt="nebaulae" src={nebulae} draggable={false}/> */}
       </div>
-      <div class="tabellDiv">
-      <p>He</p>
+      <div className="tabellDiv">
+      <table>
+          <thead>
+          <tr>
+              <th>Type</th>
+              <th>Name</th>
+              <th>Moons</th>
+              <th>Temperature</th>
+              <th>Discovered</th>
+              <th>Gravity</th>
+          </tr>
+          {planets.map((e)=>(<tr><td key={e.id}>{e.name}</td></tr>))}
+          </thead>
+      </table>
       </div>
     </div>
   );
