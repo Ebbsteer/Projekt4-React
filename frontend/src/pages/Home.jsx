4bfb2588e0028165f5327bacbf21eddaa99789b3
zimonjohnsson <zimonjohnsson2004@gmail.com>
2023-09-29 10:49:30 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import nebulae from "../assets/img/nebulae.jpeg";
 
 const Home = () => {
+
+    const [num, setNum] = useState(0);
+
+    const randomNumberInRange = (min, max) => {
+      return Math.floor(Math.random()
+      *(max - min + 1)) + min;
+    };
+
+  const handleClick=()=>{
+    setNum(randomNumberInRange(1,8))
+    if(num==1){
+      window.location = `/planet/mercury`
+    }
+  };
+
   return (
     <div id="home">
       <div className="nebulae">
         {/* <img className="nebaulae" alt="nebaulae" src={nebulae} draggable={false}/> */}
       </div>
       <div className="Homebox">
+        <h2>Number is: {num}</h2>
+      <button onClick={handleClick}>
+                Click Me Generate
+            </button>
         <div className="textbox">
           <h1>INFORMATION ABOUT PLANETS</h1>
           <p>Did you know that Venus rotates in the opposite 
