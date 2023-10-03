3a3e2aa54f5f7af88e5ce75c228f3bd8cdba00df
zimonjohnsson <zimonjohnsson2004@gmail.com>
2023-10-03 09:41:43 +0200

    hgf

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import nebulae from "../assets/img/nebulae.jpeg";
-import { useParams } from "react-router-dom";
 
 const images = [
   '/src/assets/img/mercurymock.png',
@@ -29,35 +28,6 @@ const Slideshow = ({ images }) => {
 };
 
 const Home = () => {
-
-  const { id } = useParams();
-  console.log(id);
-
-  const itemList = "https://api.le-systeme-solaire.net/rest/bodies/";
-
-  const [planets, setPlanets] = useState([]);
-  const [dag, setDag] = useState("");
-
-  const fetchUserData = () => {
-    fetch(itemList)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setPlanets(data.bodies);
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-  };
-
-  useEffect(() => {
-    fetchUserData();
-  }, []);
 
   var now = new Date();
   var start = new Date(now.getFullYear(), 0, 0);
