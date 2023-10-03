fee9ca09ed32cba1cba7f59db98cc2168a855120
Salah Al Hassun <142593904+Salah9320Alhassun@users.noreply.github.com>
2023-10-03 14:57:23 +0200

    profile page responsive and background added

--- frontend/src/pages/profil.jsx
+++ frontend/src/pages/profil.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
-import profil from "../assets/img/profilwallpaper.jpeg";
-
-
+import React from 'react';
+import profil from "../assets/img/profilwallpaper.gif";
 
 const Profil = () => {
-    return <>Profil</>;
+    return (
+        <div id="profil">
+            <img className="profilwallpaper" src={profil} alt="Profile Wallpaper" />
+            {/* Other content for your Profil component */}
+        </div>
+    );
 };
 
 export default Profil;
