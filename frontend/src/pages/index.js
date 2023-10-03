31bb6075088691f2ef3c9f4f75722ae9ecfebdff
Salah Al Hassun <142593904+Salah9320Alhassun@users.noreply.github.com>
2023-10-03 09:34:51 +0200

    login sida klar o profil sidan created

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,3 +4,4 @@ export { default as Planet } from "./Planet";
 export { default as Items } from "./Items";
 export { default as Login } from "./login";
 export { default as Exempel } from "./exempel";
+export { default as Profil } from "./profil";
