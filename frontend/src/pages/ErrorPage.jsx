7b0a46d76e576bff44699af0f1b206fa92caf7ba
Ebbsteer <Ebba.elversson@gmail.com>
2023-10-03 09:59:28 +0200

    fix + Errorpage

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,5 +1,18 @@
+import React, { useEffect, useState } from "react";
+import nebulae from "../assets/img/nebulae.jpeg";
+
+
 const ErrroPage = () => {
-    return <>ErrroPage</>;
+    return <>
+        <div id="errorpage">
+            <div className="Message">
+
+            </div>
+            <div className="btm-backtohome">
+                
+            </div>
+        </div>
+    </>;
 };
 
 export default ErrroPage;
