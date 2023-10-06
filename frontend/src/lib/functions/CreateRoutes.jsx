0cac04602d8a16ac265f5b3fd6f899cf475213c6
David Olsson <david.olsson@elev.ntig.se>
2023-10-06 10:56:19 +0200

    Goober fix

--- frontend/src/lib/functions/CreateRoutes.jsx
+++ frontend/src/lib/functions/CreateRoutes.jsx
@@ -5,7 +5,15 @@ import {
     Outlet,
 } from "react-router-dom";
 
-import { Home, ErrorPage, Planet, Items, Login, Exempel, Profil } from "../../pages";
+import {
+    Home,
+    ErrorPage,
+    Planet,
+    Items,
+    Login,
+    Exempel,
+    Profil,
+} from "../../pages";
 import { Navbar, Footer } from "../../components";
 
 const OutletTest = () => {
@@ -27,8 +35,8 @@ const CreateRoutes = () => {
                         <Route index element={<Home />} />
                         <Route path="*" element={<ErrorPage />} />
                         <Route path="/planet/:id" element={<Planet />} />
-                        <Route path="/items" element={<Items />} />
-                        <Route path="/login/:id" element={<Login />} />
+                        <Route path="/items/:id?" element={<Items />} />
+                        <Route path="/login" element={<Login />} />
                         <Route path="/exempel" element={<Exempel />} />
                         <Route path="/profil" element={<Profil />} />
                     </Route>
--- frontend/src/pages/items.jsx
+++ frontend/src/pages/items.jsx
@@ -2,265 +2,290 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Items = () => {
-  const [activeButtons, setActiveButtons] = useState({});
+    const itemList = "https://api.le-systeme-solaire.net/rest/bodies/";
 
+    const [activeButtons, setActiveButtons] = useState({});
 
+    const [planets, setPlanets] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const [sortOrder, setSortOrder] = useState("asc");
+    const [sortOrderDate, setSortOrderDate] = useState("asc");
+    const [sortOrderType, setSortOrderType] = useState("asc");
 
-  const toggleClass = (planetId) => {
-    const updatedButtons = { ...activeButtons };
-    updatedButtons[planetId] = !updatedButtons[planetId];
-    setActiveButtons(updatedButtons);
+    const [filterType, setFilterType] = useState("all"); // Default to "all"
+    const [showFavorites, setShowFavorites] = useState(false);
 
-    localStorage.setItem("activeButtons", JSON.stringify(updatedButtons));
-  };
+    const toggleClass = (planetId) => {
+        const updatedButtons = { ...activeButtons };
+        updatedButtons[planetId] = !updatedButtons[planetId];
+        setActiveButtons(updatedButtons);
 
-  const handleButtonClick = (planetId, planetI) => {
-    toggleClass(planetId);
+        localStorage.setItem("activeButtons", JSON.stringify(updatedButtons));
+    };
 
-    fetch("http://localhost:3000/add-favorite", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(planetId),
-    })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-    console.log(planetId, planetI);
-  };
+    const handleButtonClick = (planetId, planetI) => {
+        toggleClass(planetId);
 
-  const { id } = useParams();
+        fetch("http://localhost:3000/add-favorite", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(planetId),
+        })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.error("Fetch error:", error);
+            });
+        console.log(planetId, planetI);
+    };
 
-  const itemList = "https://api.le-systeme-solaire.net/rest/bodies/";
+    const { id } = useParams();
 
-  const [planets, setPlanets] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-  const [sortOrder, setSortOrder] = useState("asc");
-  const [sortOrderDate, setSortOrderDate] = useState("asc");
-  const [sortOrderType, setSortOrderType] = useState("asc");
+    useEffect(() => {
+        const storedButtons =
+            JSON.parse(localStorage.getItem("activeButtons")) || {};
+        setActiveButtons(storedButtons);
 
-  const [filterType, setFilterType] = useState("all"); // Default to "all"
-  const [showFavorites, setShowFavorites] = useState(false);
+        console.log(id);
 
-  useEffect(() => {
-    const storedButtons =
-      JSON.parse(localStorage.getItem("activeButtons")) || {};
-    setActiveButtons(storedButtons);
-  }, []);
+        if (id === "fav") setShowFavorites(!showFavorites);
+    }, []);
 
-  const sortlistName = () => {
-    const newSortOrder = sortOrder === "desc" ? "asc" : "desc";
-    setSortOrder(newSortOrder);
+    const sortlistName = () => {
+        const newSortOrder = sortOrder === "desc" ? "asc" : "desc";
+        setSortOrder(newSortOrder);
 
-    // Sort the searchResults array based on the sorting order
-    const sortedResults = [...searchResults].sort((a, b) => {
-      const nameA = a.englishName.toLowerCase();
-      const nameB = b.englishName.toLowerCase();
+        // Sort the searchResults array based on the sorting order
+        const sortedResults = [...searchResults].sort((a, b) => {
+            const nameA = a.englishName.toLowerCase();
+            const nameB = b.englishName.toLowerCase();
 
-      if (newSortOrder === "asc") {
-        return nameA.localeCompare(nameB);
-      } else {
-        return nameB.localeCompare(nameA);
-      }
-    });
+            if (newSortOrder === "asc") {
+                return nameA.localeCompare(nameB);
+            } else {
+                return nameB.localeCompare(nameA);
+            }
+        });
 
-    setSearchResults(sortedResults);
-  };
-  const sortlistDate = () => {
-    // Toggle sorting order for Date column
-    const newSortOrder = sortOrderDate === "desc" ? "asc" : "desc";
-    setSortOrderDate(newSortOrder);
+        setSearchResults(sortedResults);
+    };
 
-    const sortedResults = [...searchResults].sort((a, b) => {
-      const dateA = new Date(parseDate(a.discoveryDate)).getTime();
-      const dateB = new Date(parseDate(b.discoveryDate)).getTime();
+    const sortlistDate = () => {
+        // Toggle sorting order for Date column
+        const newSortOrder = sortOrderDate === "desc" ? "asc" : "desc";
+        setSortOrderDate(newSortOrder);
 
-      if (newSortOrder === "asc") {
-        return dateA - dateB;
-      } else {
-        return dateB - dateA;
-      }
-    });
+        const sortedResults = [...searchResults].sort((a, b) => {
+            const dateA = new Date(parseDate(a.discoveryDate)).getTime();
+            const dateB = new Date(parseDate(b.discoveryDate)).getTime();
 
-    setSearchResults(sortedResults);
-  };
+            if (newSortOrder === "asc") {
+                return dateA - dateB;
+            } else {
+                return dateB - dateA;
+            }
+        });
 
-  const sortlistType = () => {
-    // Toggle sorting order for Type column
-    const newSortOrder = sortOrderType === "desc" ? "asc" : "desc";
-    setSortOrderType(newSortOrder);
+        setSearchResults(sortedResults);
+    };
 
-    const sortedResults = [...searchResults].sort((a, b) => {
-      const typeA = a.bodyType.toLowerCase();
-      const typeB = b.bodyType.toLowerCase();
+    const sortlistType = () => {
+        // Toggle sorting order for Type column
+        const newSortOrder = sortOrderType === "desc" ? "asc" : "desc";
+        setSortOrderType(newSortOrder);
 
-      if (newSortOrder === "asc") {
-        return typeA.localeCompare(typeB);
-      } else {
-        return typeB.localeCompare(typeA);
-      }
-    });
+        const sortedResults = [...searchResults].sort((a, b) => {
+            const typeA = a.bodyType.toLowerCase();
+            const typeB = b.bodyType.toLowerCase();
 
-    setSearchResults(sortedResults);
-  };
+            if (newSortOrder === "asc") {
+                return typeA.localeCompare(typeB);
+            } else {
+                return typeB.localeCompare(typeA);
+            }
+        });
 
-  const handleTypeFilterChange = (e) => {
-    setFilterType(e.target.value);
-  };
+        setSearchResults(sortedResults);
+    };
 
-  const handleFavoritesFilterChange = () => {
-    setShowFavorites(!showFavorites);
-  };
+    const handleTypeFilterChange = (e) => {
+        setFilterType(e.target.value);
+    };
 
-  const filteredResults = searchResults.filter((planet) => {
-    const isFavorite = activeButtons[planet.id];
-    if (showFavorites && !isFavorite) {
-      return false;
-    }
-    if (filterType === "all" || planet.bodyType.toLowerCase() === filterType) {
-      return true;
-    }
-    return false;
-  });
+    const handleFavoritesFilterChange = () => {
+        setShowFavorites(!showFavorites);
+    };
 
-  const fetchUserData = () => {
-    fetch(itemList)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
+    const filteredResults = searchResults.filter((planet) => {
+        const isFavorite = activeButtons[planet.id];
+        if (showFavorites && !isFavorite) {
+            return false;
         }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setPlanets(data.bodies);
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-  };
+        if (
+            filterType === "all" ||
+            planet.bodyType.toLowerCase() === filterType
+        ) {
+            return true;
+        }
+        return false;
+    });
 
-  useEffect(() => {
-    fetchUserData();
-  }, []);
+    const fetchUserData = () => {
+        fetch(itemList)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setPlanets(data.bodies);
+            })
+            .catch((error) => {
+                console.error("Fetch error:", error);
+            });
+    };
 
-  useEffect(() => {
-    const filteredData = planets.filter((item) =>
-      item.englishName.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setSearchResults(filteredData);
-  }, [planets, searchTerm]);
+    useEffect(() => {
+        fetchUserData();
+    }, []);
 
-  const parseDate = (dateString) => {
-    const parts = dateString.split("/");
-    if (parts.length === 3) {
-      const [day, month, year] = parts;
-      return `${year}-${month}-${day}`;
-    } else {
-      // Return a default date if the input format is not as expected
-      return "1970-01-01"; // You can choose any default date you prefer
-    }
-  };
+    useEffect(() => {
+        const filteredData = planets.filter((item) =>
+            item.englishName.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setSearchResults(filteredData);
+    }, [planets, searchTerm]);
 
+    const parseDate = (dateString) => {
+        const parts = dateString.split("/");
+        if (parts.length === 3) {
+            const [day, month, year] = parts;
+            return `${year}-${month}-${day}`;
+        } else {
+            // Return a default date if the input format is not as expected
+            return "1970-01-01"; // You can choose any default date you prefer
+        }
+    };
 
-  if (id === "fav") {
-    handleFavoritesFilterChange();
+    return (
+        <div id="home">
+            <div className="nebulae"></div>
+            <div className="tabellDiv">
+                <div className="filter">
+                    <input
+                        className="search"
+                        type="text"
+                        placeholder="Sök..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
 
-    console.log("hej hej");
-  }
-  
-  
-  return (
-    <div id="home">
-      <div className="nebulae"></div>
-      <div className="tabellDiv">
-     <div className="filter"> 
-          <input
-            className="search"
-            type="text"
-            placeholder="Sök..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-
-        
-            <select className="type" value={filterType} onChange={handleTypeFilterChange}>
-              <option value="all">All Types</option>
-              <option value="star">Star</option>
-              <option value="planet">Planets</option>
-              <option value="moon">Moons</option>
-              <option value="dwarf planet">Dwarf planets</option>
-              <option value="asteroid">Asteroids</option>
-              <option value="comet">Comets</option>
-            </select>
-
-            <label className="type">
-              <input
-                type="checkbox"
-                checked={showFavorites}
-                onChange={handleFavoritesFilterChange}
-              />
-              Show Favorites
-            </label>
-            </div>
-   
-        
-        <table className="itemtable">
-          <thead>
-            <tr>
-              <th className="item-table-title-favo"></th>
-              <th className="item-table-title-nr">Nr</th>
-              <th onClick={sortlistType} className="item-table-title-type">
-                Type {sortOrderType === "asc" ? "▼" : "▲"}{" "}
-              </th>
-              <th onClick={sortlistName} className="item-table-title-name">
-                Name {sortOrder === "asc" ? "▼" : "▲"}
-              </th>
-              <th className="item-table-title-temp">Temperature- (K)</th>
-              <th className="item-table-title-grav">Gravity</th>
-              <th onClick={sortlistDate} className="item-table-title-disc">
-                Discovered {sortOrderDate === "asc" ? "▼" : "▲"}{" "}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredResults.length === 0 ? (
-              <tr>
-                <td colSpan="5">No matching cosmic body</td>
-              </tr>
-            ) : (
-              filteredResults.map((planet, i) => (
-                <tr key={planet.id}>
-                  <td className="item-table-info-favo">
-                    <button
-                      id="favoriteButton"
-                      className={activeButtons[planet.id] ? "makeFavorite" : ""}
-                      onClick={() => handleButtonClick(planet.id, i)}
+                    <select
+                        className="type"
+                        value={filterType}
+                        onChange={handleTypeFilterChange}
                     >
-                      <p id="btnContent">&#9733;</p>
-                    </button>
-                  </td>
-                  <td className="item-table-info-nr">{i}</td>
-                  <td className="item-table-info-type">{planet.bodyType}</td>
-                  <td className="item-table-info-name">{planet.englishName}</td>
-                  <td className="item-table-info-temp">{planet.avgTemp}</td>
-                  <td className="item-table-info-grav">{planet.gravity}</td>
-                  <td className="item-table-info-disc">
-                    {planet.discoveryDate}
-                  </td>
-                </tr>
-              ))
-            )}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+                        <option value="all">All Types</option>
+                        <option value="star">Star</option>
+                        <option value="planet">Planets</option>
+                        <option value="moon">Moons</option>
+                        <option value="dwarf planet">Dwarf planets</option>
+                        <option value="asteroid">Asteroids</option>
+                        <option value="comet">Comets</option>
+                    </select>
+
+                    <label className="type">
+                        <input
+                            type="checkbox"
+                            checked={showFavorites}
+                            onChange={handleFavoritesFilterChange}
+                        />
+                        Show Favorites
+                    </label>
+                </div>
+
+                <table className="itemtable">
+                    <thead>
+                        <tr>
+                            <th className="item-table-title-favo"></th>
+                            <th className="item-table-title-nr">Nr</th>
+                            <th
+                                onClick={sortlistType}
+                                className="item-table-title-type"
+                            >
+                                Type {sortOrderType === "asc" ? "▼" : "▲"}{" "}
+                            </th>
+                            <th
+                                onClick={sortlistName}
+                                className="item-table-title-name"
+                            >
+                                Name {sortOrder === "asc" ? "▼" : "▲"}
+                            </th>
+                            <th className="item-table-title-temp">
+                                Temperature- (K)
+                            </th>
+                            <th className="item-table-title-grav">Gravity</th>
+                            <th
+                                onClick={sortlistDate}
+                                className="item-table-title-disc"
+                            >
+                                Discovered {sortOrderDate === "asc" ? "▼" : "▲"}{" "}
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredResults.length === 0 ? (
+                            <tr>
+                                <td colSpan="5">No matching cosmic body</td>
+                            </tr>
+                        ) : (
+                            filteredResults.map((planet, i) => (
+                                <tr key={planet.id}>
+                                    <td className="item-table-info-favo">
+                                        <button
+                                            id="favoriteButton"
+                                            className={
+                                                activeButtons[planet.id]
+                                                    ? "makeFavorite"
+                                                    : ""
+                                            }
+                                            onClick={() =>
+                                                handleButtonClick(planet.id, i)
+                                            }
+                                        >
+                                            <p id="btnContent">&#9733;</p>
+                                        </button>
+                                    </td>
+                                    <td className="item-table-info-nr">{i}</td>
+                                    <td className="item-table-info-type">
+                                        {planet.bodyType}
+                                    </td>
+                                    <td className="item-table-info-name">
+                                        {planet.englishName}
+                                    </td>
+                                    <td className="item-table-info-temp">
+                                        {planet.avgTemp}
+                                    </td>
+                                    <td className="item-table-info-grav">
+                                        {planet.gravity}
+                                    </td>
+                                    <td className="item-table-info-disc">
+                                        {planet.discoveryDate}
+                                    </td>
+                                </tr>
+                            ))
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default Items;
