9b7c0b2e919450013b7b034f6ca62012a3b84aab
Salah Al Hassun <142593904+Salah9320Alhassun@users.noreply.github.com>
2023-10-06 09:20:03 +0200

    update login/register

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -6,9 +6,12 @@ const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [retypePassword, setRetypePassword] = useState("");
+    const [securityQuestion, setSecurityQuestion] = useState("");
     const [rememberMe, setRememberMe] = useState(false);
     const [isRegistering, setIsRegistering] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
+    const [resettingPassword, setResettingPassword] = useState(false);
+    const [resetPasswordUsername, setResetPasswordUsername] = useState(""); // New state for resetting password username
 
     const handleLogin = () => {
         // Prepare the login data as an object
@@ -46,20 +49,44 @@ const Login = () => {
     };
 
     const handleForgotPassword = () => {
-        // Implement logic for password reset here
+        setResettingPassword(true); // Set the state to indicate password reset
+        setUsername(""); // Clear the username field
+    };
+
+    const handleResetPassword = () => {
+        // Retrieve the security question and answer from localStorage based on the username
+        const storedData = localStorage.getItem(resetPasswordUsername);
+
+        if (storedData) {
+            const { securityQuestion: storedSecurityQuestion } = JSON.parse(storedData);
+
+            if (securityQuestion === storedSecurityQuestion) {
+                // Implement your password reset logic here
+                alert("Password reset successful");
+                setResettingPassword(false); // Reset the state to hide the reset form
+            } else {
+                alert("Incorrect security answer");
+            }
+        } else {
+            alert("Username not found");
+        }
     };
 
     const handleRegister = (e) => {
-        e.preventDefault(); // Förhindra standardbeteendet för knappen
+        e.preventDefault(); // Prevent the default behavior of the button
         setIsRegistering(!isRegistering);
 
-        // Prepare the login data as an object
+        // Prepare the registration data as an object
         const registerData = {
             username,
             password,
+            securityQuestion,
         };
 
         console.log(registerData);
+
+        // Store registration data in localStorage
+        localStorage.setItem(username, JSON.stringify(registerData));
 
         fetch("http://localhost:3000/register", {
             method: "POST",
@@ -86,62 +113,91 @@ const Login = () => {
             <div className="spacepic">{/* Background image */}</div>
             <div className="login-container">
                 <h2>{isRegistering ? "Register" : "Login"}</h2>
-                <form className="login-form">
-                    <input
-                        type="text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <div className="password-input">
+                {resettingPassword ? ( // Display password reset form when resettingPassword is true
+                    <form className="reset-password-form">
                         <input
-                            type={showPassword ? "text" : "password"}
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            type="text"
+                            placeholder="Username"
+                            value={resetPasswordUsername}
+                            onChange={(e) => setResetPasswordUsername(e.target.value)}
                         />
+                        <input
+                            type="text"
+                            placeholder="Security Question: What is your mom's name?"
+                            value={securityQuestion}
+                            onChange={(e) => setSecurityQuestion(e.target.value)}
+                        />
+                        <button type="button" onClick={handleResetPassword}>
+                            Reset Password
+                        </button>
+                    </form>
+                ) : (
+                    // Display login/registration form when resettingPassword is false
+                    <form className="login-form">
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <div className="password-input">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setShowPassword(!showPassword)}
+                                className="show-password-button"
+                            >
+                                {showPassword ? "Hide" : "Show"}
+                            </button>
+                        </div>
+                        {isRegistering && (
+                            <>
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder="Re-type Password"
+                                    value={retypePassword}
+                                    onChange={(e) => setRetypePassword(e.target.value)}
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="Security Question: What is your mom's name?"
+                                    value={securityQuestion}
+                                    onChange={(e) => setSecurityQuestion(e.target.value)}
+                                />
+                            </>
+                        )}
                         <button
                             type="button"
-                            onClick={() => setShowPassword(!showPassword)}
-                            className="show-password-button"
+                            onClick={isRegistering ? handleRegister : handleLogin}
                         >
-                            {showPassword ? "Hide" : "Show"}
+                            {isRegistering ? "Register" : "Login"}
                         </button>
-                    </div>
-                    {isRegistering && (
-                        <input
-                            type={showPassword ? "text" : "password"}
-                            placeholder="Re-type Password"
-                            value={retypePassword}
-                            onChange={(e) => setRetypePassword(e.target.value)}
-                        />
-                    )}
-                    <button
-                        type="button"
-                        onClick={isRegistering ? handleRegister : handleLogin}
-                    >
-                        {isRegistering ? "Register" : "Login"}
-                    </button>
-                    {!isRegistering && (
-                        <button type="button" onClick={handleForgotPassword}>
-                            Forgot Password
+                        {!isRegistering && (
+                            <button type="button" onClick={handleForgotPassword}>
+                                Forgot Password
+                            </button>
+                        )}
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={rememberMe}
+                                onChange={() => setRememberMe(!rememberMe)}
+                            />
+                            Remember Me
+                        </label>
+                        <button
+                            className="register-button"
+                            onClick={handleRegister}
+                        >
+                            {isRegistering ? "Back to Login" : "Register here"}
                         </button>
-                    )}
-                    <label>
-                        <input
-                            type="checkbox"
-                            checked={rememberMe}
-                            onChange={() => setRememberMe(!rememberMe)}
-                        />
-                        Remember Me
-                    </label>
-                    <button
-                        className="register-button"
-                        onClick={handleRegister}
-                    >
-                        {isRegistering ? "Back to Login" : "Register here"}
-                    </button>
-                </form>
+                    </form>
+                )}
             </div>
         </div>
     );
