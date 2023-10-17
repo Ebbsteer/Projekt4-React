8c18b044084059386b699ac4c5c0f51171e345c6
David Olsson <david.olsson@elev.ntig.se>
2023-10-17 12:30:50 +0200

    Fixed footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -26,37 +26,32 @@ const Footer = () => {
         <>
             <footer id="footer">
                 <div className="inner">
-                    <div className="footer-logo">
-                        <h2 className="logo">
-                            Cosmic<span className="hubStyle">Hub</span>
-                        </h2>
-                    </div>
-
                     <div className="footer-content">
+                        <h2 className="logo">CosmicHub</h2>
                         <div>
-                            <h3>Sitemap</h3>
-                            <NavLink to="/">Home</NavLink>
-                            <NavLink to="/items">Items</NavLink>
-                            <NavLink to="/login">Login/Register</NavLink>
-                        </div>
+                            <div>
+                                <h3>Sitemap</h3>
+                                <NavLink to="/">Home</NavLink>
+                                <NavLink to="/items">Items</NavLink>
+                                <NavLink to="/login">Login/Register</NavLink>
+                            </div>
 
-                        <div className="footer-planets">
-                            <h3>Planets</h3>
-                            <NavLink to="/planet/mercury">Mercury</NavLink>
-                            <NavLink to="/planet/venus">Venus</NavLink>
-                            <NavLink to="/planet/earth">Tellus</NavLink>
-                            <NavLink to="/planet/mars">Mars</NavLink>
-                            <NavLink to="/planet/jupiter">Jupiter</NavLink>
-                            <NavLink to="/planet/saturn">Saturn</NavLink>
-                            <NavLink to="/planet/uranus">Uranus</NavLink>
-                            <NavLink to="/planet/neptune">Neptune</NavLink>
+                            <div className="footer-planets">
+                                <h3>Planets</h3>
+                                <NavLink to="/planet/mercury">Mercury</NavLink>
+                                <NavLink to="/planet/venus">Venus</NavLink>
+                                <NavLink to="/planet/earth">Tellus</NavLink>
+                                <NavLink to="/planet/mars">Mars</NavLink>
+                                <NavLink to="/planet/jupiter">Jupiter</NavLink>
+                                <NavLink to="/planet/saturn">Saturn</NavLink>
+                                <NavLink to="/planet/uranus">Uranus</NavLink>
+                                <NavLink to="/planet/neptune">Neptune</NavLink>
+                            </div>
                         </div>
                     </div>
 
-                    <div>
-                        <p className="footer-CO-copyright">
-                            © 2024 CosmicHub - All rights reserved.
-                        </p>
+                    <div className="copyright-container">
+                        <p>© 2024 CosmicHub - All rights reserved.</p>
                     </div>
                 </div>
             </footer>
